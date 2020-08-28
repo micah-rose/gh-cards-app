51146b1850a2f30c9db0b89202dbbbad2e3ae893
Micah Lund <micah.r.lund@gmail.com>
2020-08-28 15:29:44 -0600

    Created a CardList component and started an array for test data.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-import Card from './components/Card';
+import CardList from './components/CardList';
 
 class App extends Component  {
   render () {
     return (
       <div className="App">
         <h1>{this.props.title}</h1>
-        <Card />
+        <CardList />
       </div>
     );
   }
