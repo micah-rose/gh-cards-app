973f04c138e2cc6b427bd9b8ea7c7b44345241a1
Micah Lund <micah.r.lund@gmail.com>
2020-08-31 15:40:51 -0600

    Moved testData to App.js and adjusted CardList not to rely on global variables.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React, {Component} from 'react';
 import CardList from './components/CardList';
 import Form from './components/Form';
 
+const testData = [
+  {name: "Dan Abramov",
+  avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4",
+  company: "Facebook"},
+  {name: "Sophie Alpert",
+  avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
+  company: "Facebook"},
+  {name: "Sebastian Markbage",
+  avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
+  company: "Facebook"}
+];
+
 class App extends Component  {
   render () {
     return (
       <div className="App">
         <h1>{this.props.title}</h1>
         <Form />
-        <CardList />
+        <CardList profiles={testData}/>
       </div>
     );
   }
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import Card from './Card';
 
-const testData = [
-    {name: "Dan Abramov",
-    avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4",
-    company: "Facebook"},
-    {name: "Sophie Alpert",
-    avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
-    company: "Facebook"},
-    {name: "Sebastian Markbage",
-    avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
-    company: "Facebook"}
-];
-
 const CardList = (props) => (
     <div>
-        {testData.map(profile => <Card {...profile}/>)}
+        {props.profiles.map(profile => <Card {...profile}/>)}
     </div>
 )
 
