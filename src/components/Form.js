5fcd9180ba4937d7ac81294e4ee8b1ad0773b840
Micah Lund <micah.r.lund@gmail.com>
2020-08-31 15:33:48 -0600

    Created a form component to take input from a user.

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Form extends Component {
+    render () {
+        return (
+            <form action="">
+                <input type="text" placeholder="GitHub Username" />
+                <button>Add Card</button>
+            </form>
+        )
+    }
+}
