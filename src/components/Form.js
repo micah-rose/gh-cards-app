3c0126e8db3e71edd3320ca4d54739f61f0142a8
Micah Lund <micah.r.lund@gmail.com>
2020-08-31 17:07:21 -0600

    Used createRef() to create a reference to the value a user inputs and tested the button functionality with a console.log.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,22 @@
-import React, {Component} from 'react';
+import React, {Component, createRef} from 'react';
 
 class Form extends Component {
 
+    userNameInput = createRef();
+
     handleSubmit = (event) => {
         event.preventDefault();
+        console.log(this.userNameInput.current.value)
     }
 
     render () {
         return (
             <div>
             <form onSubmit={this.handleSubmit}>
-                <input type="text" placeholder="GitHub Username" required />
+                <input 
+                type="text" 
+                placeholder="GitHub Username" 
+                ref={this.userNameInput} required />
                 <button>Add Card</button>
             </form>
             <br />
