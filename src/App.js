eb8d539e4ab890195663de6856cd5b24104306b1
Micah Lund <micah.r.lund@gmail.com>
2020-08-31 15:37:01 -0600

    Added the input form to App.js and wrapped form in a div so that I could include a line break tag.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import CardList from './components/CardList';
+import Form from './components/Form';
 
 class App extends Component  {
   render () {
     return (
       <div className="App">
         <h1>{this.props.title}</h1>
+        <Form />
         <CardList />
       </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,10 +3,15 @@ import React, {Component} from 'react';
 class Form extends Component {
     render () {
         return (
+            <div>
             <form action="">
                 <input type="text" placeholder="GitHub Username" />
                 <button>Add Card</button>
             </form>
+            <br />
+            </div>
         )
     }
 }
+
+export default Form;
